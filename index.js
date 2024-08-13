e772de80edbdf7213979b8867b1e868ed11141fc
mikal1995 <mikalefrs@gmail.com>
2024-08-13 08:27:05 -0400

    backend api

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
+const app = express();
+app.use(cors({ origin: true }));
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success!",
+  });
+});
+
+app.post("/payment/create", async (req, res) => {
+  const total = req.query.total;
+
+  try {
+    if (total > 0) {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+      });
+      console.log(paymentIntent);
+
+      res.status(201).json({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } else {
+      res.status(400).json({
+        message: "Total must be greater than 0",
+      });
+    }
+  } catch (error) {
+    console.error("Error creating payment intent:", error.message);
+    res.status(500).json({
+      error: "Error creating payment intent",
+    });
+  }
+});
+
+app.listen(5001, (err) => {
+  if (err) throw err;
+  console.log("Amazon Server Running on PORT: 5001, http://localhost:5001");
+});
